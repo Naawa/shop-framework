88ee3bac71e315e8745d32806b868d58ccaf5561
John <johnjjbaltazar@icloud.com>
2023-12-11 17:34:10 -0800

    Testing footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Footer from "$lib/components/Footer.svelte";
 	import Navigation from "$lib/components/Navigation.svelte";
     import "$lib/scss/styles.scss"
-    Navigation
+    
 </script>
 
 <Navigation></Navigation>
+<Footer></Footer>
 <slot />
