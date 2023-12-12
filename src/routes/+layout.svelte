0021d265fccb7a1a5a7f00e6832d13e674324f67
Naous Islam <naous.islam@outlook.com>
2023-12-11 17:00:28 -0800

    Made a bunch of routes.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import Navigation from "$lib/components/Navigation.svelte";
     import "$lib/scss/styles.scss"
+    Navigation
 </script>
 
+<Navigation></Navigation>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
     import "$lib/components/Navigation.svelte"
-    import "$lib/routes/Landing.svelte"
-	import Landing from "$lib/routes/Landing.svelte";
 </script>
 
-<Landing />
