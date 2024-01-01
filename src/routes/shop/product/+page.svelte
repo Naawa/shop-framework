1d122ed0f4870beaef1be84d9108e259ea564740
Naous Islam <naous.islam@outlook.com>
2023-12-31 22:31:15 -0800

    Idk what I am doing but this page is now responsive...

--- src/routes/shop/product/+page.svelte
+++ src/routes/shop/product/+page.svelte
@@ -11,8 +11,8 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        gap: 16em;
-        height: clamp(800px, calc(100dvh - 4em), 1280px);
+        gap: 8em;
+        min-height: calc(100dvh - 4em);
 
         div {
             display: flex;
@@ -20,26 +20,101 @@
             justify-content: center;
 
             img {
-                height: 512px;
-                width: 384px;
+                max-height: 60dvh;
+                width: 100%;
+                max-width: 60dvh;
+                box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.05);
             }
 
             &:last-of-type {
                 flex-direction: column;
                 align-items: start;
                 gap: 4em;
-                border: solid 1px red;
 
                 span {
+                    width: 100%;
                     div {
                         display: flex;
                         flex-direction: row;
-                        gap: 8em;
+                        justify-content: space-between;
+                        align-items: center;
+                        width: 100%;
+
+                        button {
+                            height: 3em;
+                            width: 3em;
+                            border-radius: 100%;
+                            border: none;
+                            display: flex;
+                            justify-content: center;
+                            align-items: center;
+                            background-color: black;
+
+                            span {
+                                &:last-of-type {
+                                    position: absolute;
+                                    width: 0.8em;
+                                    border: solid 1px white;
+                                    rotate: 90deg;
+                                }
+
+                                &:first-of-type {
+                                    width: 0.8em;
+                                    border: solid 1px white;
+                                }
+                            }
+                        }
+
+
                     }
 
                     &:last-of-type {
+                        width: 100%;
                         display: flex;
                         justify-content: space-between;
+                        gap: 8em;
+
+                        div {
+                            width: 10em;
+                            gap: 0;
+                        }
+                        
+                        button {
+                            &:only-of-type {
+                                background-color: black;
+                                border: none;
+                                border-radius: 16px;
+                                padding: 1.5em 4em;
+                                
+                                h5 {
+                                    color: white;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @media (width < 1600px) {
+        section {
+            flex-direction: column;
+            padding-bottom: 2em;
+            gap: 2em;
+
+            div {
+                width: 100%;
+                &:last-of-type {
+                    gap: 2em;
+                    span {
+                        div {
+                            gap: 0;
+                        }
+                        &:last-of-type {
+                            flex-direction: column;
+                            gap: 2em;
+                        }
                     }
                 }
             }
@@ -54,8 +129,8 @@
     <div>
         <span>
             <div>
-                <h2>{name}</h2>
-                <h2>${price}</h2>
+                <h3>{name}</h3>
+                <h3>${price}</h3>
             </div>
             <p>{detail}</p>
         </span>
@@ -64,11 +139,16 @@
         </span>
         <span>
             <div>
-                <button></button>
+                <button>
+                    <span></span>
+                </button>
                 <h3>1</h3>
-                <button></button>
+                <button>
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
-            <button>ADD TO CART</button>
+            <button><h5>ADD TO CART</h5></button>
         </span>
     </div>
 </section>
